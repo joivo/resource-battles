bc4cc107b4edd2469d86d32d31b1659821a7109f
ramonfragoso <jose.ramon.silva@ccc.ufcg.edu.br>
2019-10-10 18:01:21 -0300

    adding threads worker to examplify the countdown interface

--- lista1/02/Main.java
+++ lista1/02/Main.java
@@ -1,49 +1,70 @@
 public class Main {
 
-	public static void main(String[] args) {
-		System.out.println("teste");
+	public static void main(String args[]) throws InterruptedException { 
+        // Let us create task that is going to  
+        // wait for four threads before it starts 
+        CountDownLatch latch = new CountDownLatch(4); 
+  
+        // Let us create four worker  
+        // threads and start them. 
+        Worker first = new Worker(1000, latch, "WORKER-1"); 
+        Worker second = new Worker(2000, latch, "WORKER-2"); 
+        Worker third = new Worker(3000, latch, "WORKER-3"); 
+        Worker fourth = new Worker(4000, latch, "WORKER-4"); 
+        first.start(); 
+        second.start(); 
+        third.start(); 
+        fourth.start(); 
 
-		CountDownLatch c1 = new CountDownLatch(1);
-		System.out.println(c1.getCounter());
-
-	}
+        // The main task waits for four threads 
+        latch.await(); 
+  
+        // Main thread has started 
+        System.out.println(Thread.currentThread().getName() + " has finished"); 
+	} 
 
 	static class CountDownLatch{
 	    private int count;
-	    /**
-	     * CountDownLatch is initialized with given count.
-	     * count specifies the number of events that must occur
-	     * before latch is released.
-	     */
+
 	    public CountDownLatch(int count) {
 	           this.count=count;
 	    }
 
-	    /**
-	     * Causes the current thread to wait until  one of the following things happens-
-	                  - latch count has down to reached 0, or
-	                  - unless the thread is interrupted.
-	     */
 	    public synchronized void await() throws InterruptedException {
-	           //If count is greater than 0, thread waits.
 	           if(count>0)
 	                  this.wait();
 	    }
 
-	    /**
-	     *  Reduces latch count by 1.
-	     *  If count reaches 0, all waiting threads are released.
-	     */
 	    public synchronized void countDown() {
-	           //decrement the count by 1.
 	           count--;
-
-	           //If count is equal to 0, notify all waiting threads.
-	           if(count == 0)
-	                  this.notifyAll();
+	           if(count <= 0)
+	                this.notifyAll();
 	    }
 	}
 
-
+    static class Worker extends Thread { 
+	    private int delay; 
+	    private CountDownLatch latch; 
+	  
+	    public Worker(int delay, CountDownLatch latch, String name) { 
+	        super(name); 
+	        this.delay = delay; 
+	        this.latch = latch; 
+	    } 
+	  
+	    @Override
+	    public void run() { 
+	        try
+	        { 
+	            Thread.sleep(delay); 
+	            latch.countDown(); 
+	            System.out.println(Thread.currentThread().getName() + " finished"); 
+	        } 
+	        catch (InterruptedException e) 
+	        { 
+	            e.printStackTrace(); 
+	        } 
+	    } 
+	} 
 
 }
