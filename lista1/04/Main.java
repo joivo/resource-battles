24c9801f3f9eebc3464d07a196dfe78d01030d45
Emanuel Joívo <emanueljoivo@gmail.com>
2019-10-16 02:22:37 -0300

    Add more tests

--- lista1/04/Main.java
+++ lista1/04/Main.java
@@ -5,67 +5,44 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
-public class Main {    
+public class Main {
 
-    public static void main(String[] args) throws InterruptedException {
-        CacheMap<String, Integer> cm = new CacheMap<>(10, 10, new HashMap<>());
-        Collection<String> keysInserted = Collections.synchronizedCollection(new LinkedList<>());
-        Random random = new Random();
+    public static void main(String[] args) {
 
-        testPut(cm, random, keysInserted);
+        if (args.length == 0) {
+            log("Running test mode");
+            log("Cache size: 10");
+            log("Timeout db commit: 10");
+            final CacheMap<String, Double> cm = new CacheMap<>(10, 10, new HashMap<>());
+            Collection<String> keysInserted = Collections.synchronizedCollection(new LinkedList<>());
 
-        testGet(cm, keysInserted);
-    }
+            testPut(cm, keysInserted);
 
-    private static void testGet(CacheMap<String, Integer> cm, Collection<String> keys) {
-        log("" + (!cm.isEmpty()));
+            testGet(cm, keysInserted);
 
-        List<Thread> getWorkers = new LinkedList<>();
+            testRemove(cm, keysInserted);
 
-        log("" + (!keys.isEmpty()));
-
-        for (String key : keys) {
-            getWorkers.add(new GetWorker<>("GetWorker-" + key, cm, key));
+            log("--- Tests finished ---");
+            System.exit(0);
+        } else {
+            log("No parameters supported yet");
+            System.exit(1);
         }
-
-        executeThreads(getWorkers, keys.size());
     }
 
-    private static void testPut(CacheMap<String, Integer> cm, Random r, Collection<String> workersKeys) {
-
-        List<Thread> putWorkers = new ArrayList<>();
-        log("" + (cm.size() == 0));
-
-        int nThreads = 11;
+    private static void testPut(CacheMap<String, Double> cm, Collection<String> workersKeys) {
+        log("--- Put tests ---");
+        // set up
         long waitTime = 10 * 1000;
 
-        generatePutWorkers(nThreads, r, workersKeys, putWorkers, cm);
-        executeThreads(putWorkers, nThreads);
+        // exercise
+        doPutWork(11, workersKeys, cm);
 
-        log("" + (cm.size() == 11));
-        log("" + (cm.cache.size() == 1));
-        log("" + (cm.db.size() == 10));
-
-        try {
-            Thread.sleep(waitTime);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        log("" + (cm.cache.size() == 1));
-        log("" + (cm.db.size() == 10));
-
-        int nThreads2 = 9;
-        List<Thread> putWorkers2 = new ArrayList<>();
-        generatePutWorkers(nThreads2, r, workersKeys, putWorkers2, cm);
-
-        executeThreads(putWorkers2, nThreads2);
-
-        log("" + (cm.size() == 20));
-
-        log("" + (cm.cache.size() == 10));
-
-        log("" + (cm.db.size() == 10));
+        // verify
+        log("Test 01 - Result: " + (cm.size() == 11));
+        log("Test 02 - Result: " + (cm.cache.size() == 1));
+        log("Test 03 - Result: " + (cm.db.size() == 10));
+        log("Test 04 - Result: " + (workersKeys.size() == 11));
 
         try {
             Thread.sleep(waitTime);
@@ -73,41 +50,177 @@ public class Main {
             e.printStackTrace();
         }
 
-        log("" + (cm.size() == 20));
+        log("Test 05 - Result: " + (cm.cache.size() == 1));
+        log("Test 06 - Result: " + (cm.db.size() == 10));
 
-        log("" + (cm.cache.size() == 0));
+        // exercise
+        doPutWork(9, workersKeys, cm);
+        log("Test 07 - Result: " + (workersKeys.size() == 20));
 
-        log("" + (cm.db.size() == 20));
+        // verify
+        log("Test 08 - Result: " + (cm.size() == 20));
+        log("Test 09 - Result: " + (cm.cache.size() == 10));
+        log("Test 10 - Result: " + (cm.db.size() == 10));
+
+        try {
+            Thread.sleep(waitTime);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        log("Test 11 - Result: " + (cm.size() == 20));
+
+        log("Test 12 - Result: " + (cm.cache.size() == 0));
+
+        log("Test 13 - Result: " + (cm.db.size() == 20));
+
+        cm.clear();
+        workersKeys.clear();
     }
 
-    private static void generatePutWorkers(int nThreads, Random r, Collection<String> workersKeys,
-            List<Thread> putWorkers, CacheMap<String, Integer> cm) {
-        for (int i = 0; i <= nThreads; i++) {
-            Integer value = Math.abs(r.nextInt());
+    private static void doPutWork(int nWorkers, Collection<String> workersKeys, CacheMap<String, Double> cm) {
+        List<Thread> putWorkers = new ArrayList<>();
+        for (int i = 0; i < nWorkers; i++) {
             String key = UUID.randomUUID().toString();
+
+            Double value = Math.random();
+
             workersKeys.add(key);
-            putWorkers.add(new PutWorker<String, Integer>("PutWorker-" + i, cm, key, value));
+            putWorkers.add(new PutWorker<>("PutWorker-" + i, cm, key, value));
         }
+
+        doWork(putWorkers);
     }
 
-    private static void generateRemoveWorkers(int nThreads, Random r, Collection<Integer> workersKeys,
-            List<Thread> removeWorkers, CacheMap<Integer, Integer> cm) {
-        for (int i = 0; i <= nThreads; i++) {
-            Integer key = r.nextInt();
-            workersKeys.add(key);
-            removeWorkers.add(new PutWorker<>("RemoveWorker-" + i, cm, key, key - 1));
+    private static void testGet(CacheMap<String, Double> cm, Collection<String> workersKeys) {
+        log("--- Get tests ---");
+        List<Thread> getWorkers = new LinkedList<>();
+
+        log("Test 14 - Result: " + (cm.isEmpty()));
+        log("Test 15 - Result: " + (workersKeys.isEmpty()));
+
+        doPutWork(20, workersKeys, cm);
+
+        for (String workerKey : workersKeys) {
+            if (cm.containsKey(workerKey)) {
+                getWorkers.add(new GetWorker<>("GetWorker-" + workerKey, cm, workerKey));
+            }
         }
+
+        doWork(getWorkers);
+
+        log("Test 16 - Result: " + (cm.size() == 20));
+
+        log("Test 17 - Result: " + (cm.cache.size() == 10));
+
+        log("Test 18 - Result: " + (cm.db.size() == 10));
+
+        try {
+            Thread.sleep(10 * 1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        log("Test 19 - Result: " + (cm.size() == 20));
+
+        log("Test 20 - Result: " + (cm.cache.size() == 0));
+
+        log("Test 21 - Result: " + (cm.db.size() == 20));
+
+        cm.clear();
+        workersKeys.clear();
     }
 
-    private static void executeThreads(List<Thread> threads, int nThreads) {
+    private static void testRemove(CacheMap<String, Double> cm, Collection<String> workersKeys) {
+        log("--- Remove tests ---");
 
-        for (int i = 0; i < nThreads; i++) {
-            threads.get(i).start();
+        log("Test 22 - Result: " + (cm.isEmpty()));
+        log("Test 23 - Result: " + (workersKeys.isEmpty()));
+
+        doPutWork(22, workersKeys, cm);
+
+        log("Test 24 - Result: " + (cm.size() == 22));
+        log("Test 25 - Result: " + (cm.cache.size() == 2));
+        log("Test 26 - Result: " + (cm.db.size() == 20));
+
+        doRemoveWork(22, workersKeys, cm);
+
+        log("Test 27 - Result: " + (cm.isEmpty()));
+        log("Test 28 - Result: " + (workersKeys.isEmpty()));
+
+        doPutWork(30, workersKeys, cm);
+
+        log("Test 29 - Result: " + (cm.size() == 30));
+        log("Test 30 - Result: " + (cm.cache.size() == 10));
+        log("Test 31 - Result: " + (cm.db.size() == 20));
+
+        try {
+            Thread.sleep(10 * 1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
 
-        for (int i = 0; i < nThreads; i++) {
+        log("Test 32 - Result: " + (cm.size() == 30));
+        log("Test 33 - Result: " + (cm.cache.size() == 0));
+        log("Test 34 - Result: " + (cm.db.size() == 30));
+
+        doRemoveWork(20, workersKeys, cm);
+
+        log("Test 35 - Result: " + (cm.size() == 10));
+        log("Test 36 - Result: " + (cm.cache.size() == 0));
+        log("Test 37 - Result: " + (cm.db.size() == 10));
+
+        doPutWork(30, workersKeys, cm);
+
+        log("Test 38 - Result: " + (cm.size() == 40));
+        log("Test 39 - Result: " + (cm.cache.size() == 10));
+        log("Test 40 - Result: " + (cm.db.size() == 30));
+
+        try {
+            Thread.sleep(10 * 1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        log("Test 41 - Result: " + (cm.size() == 40));
+        log("Test 42 - Result: " + (cm.cache.size() == 0));
+        log("Test 43 - Result: " + (cm.db.size() == 40));
+
+        doRemoveWork(40, workersKeys, cm);
+
+        log("Test 44 - Result: " + (cm.isEmpty()));
+        log("Test 45 - Result: " + (workersKeys.isEmpty()));
+    }
+
+    private static void doRemoveWork(int nWorkers, Collection<String> workersKeys, CacheMap<String, Double> cm) {
+        List<Thread> removeWorkers = new ArrayList<>();
+        List<String> removedKeys = new ArrayList<>();
+
+        int i = 0;
+        for (String workerKey : workersKeys) {
+            if (i < nWorkers) {
+                i++;
+                removeWorkers.add(new RemoveWorker<>("RemoveWorker-" + workerKey, cm, workerKey));
+                removedKeys.add(workerKey);
+            } else
+                break;
+        }
+
+        for (String workerKey : removedKeys)
+            workersKeys.remove(workerKey);
+
+        doWork(removeWorkers);
+    }
+
+    private static void doWork(List<Thread> workers) {
+
+        for (int i = 0; i < workers.size(); i++) {
+            workers.get(i).start();
+        }
+
+        for (int i = 0; i < workers.size(); i++) {
             try {
-                threads.get(i).join();
+                workers.get(i).join();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
@@ -151,42 +264,49 @@ public class Main {
             return size() == 0;
         }
 
-        synchronized boolean containsKey(K k) {
-            return this.cache.containsKey(k) ? this.cache.containsKey(k) : this.db.containsKey(k);
+        boolean containsKey(K k) {
+            synchronized (this) {
+                return this.cache.containsKey(k) ? this.cache.containsKey(k) : this.db.containsKey(k);
+            }
         }
 
-        synchronized V get(K k) throws InterruptedException {
-            if (this.cache.containsKey(k)) {
-                return this.cache.get(k);
-            } else {
-                if (this.db.containsKey(k)) {
-                    if (!cacheIsFull()) {
-                        this.wait();
-                        return putOnCache(k);
+        V get(K k) throws InterruptedException {
+            synchronized (this) {
+                if (this.cache.containsKey(k)) {
+                    return this.cache.get(k);
+                } else {
+                    if (this.db.containsKey(k)) {
+                        if (cacheIsFull()) {
+                            this.wait();
+                            return putOnCache(k);
+                        }
                     }
                 }
+                return null;
             }
-            return null;
         }
 
         V put(K k, V v) {
-            if (cacheIsFull()) {
-                commit();
-            }
-            synchronized (this.cache) {
-                return this.cache.put(k, v);
+            synchronized (this) {
+                if (cacheIsFull()) {
+                    commit();
+                }
+                this.cache.put(k, v);
+                return this.cache.get(k);
             }
         }
 
-        synchronized V remove(K k) {
-            if (this.cache.containsKey(k)) {
-                return this.cache.remove(k);
-            } else {
-                if (this.db.containsKey(k)) {
-                    return this.db.remove(k);
+        V remove(K k) {
+            synchronized (this) {
+                if (this.cache.containsKey(k)) {
+                    return this.cache.remove(k);
+                } else {
+                    if (this.db.containsKey(k)) {
+                        return this.db.remove(k);
+                    }
                 }
+                return null;
             }
-            return null;
         }
 
         void clear() {
@@ -196,23 +316,30 @@ public class Main {
             }
         }
 
-        private synchronized V putOnCache(K k) {
-            V v = this.db.remove(k);
-            return this.cache.put(k, v);
+        private V putOnCache(K k) {
+            synchronized (this) {
+                V v = this.db.remove(k);
+                this.cache.put(k, v);
+                return this.cache.get(k);
+            }
         }
 
-        private synchronized boolean cacheIsFull() {
-            return this.cache.size() == this.cacheSize;
+        private boolean cacheIsFull() {
+            synchronized (this) {
+                return this.cache.size() == this.cacheSize;
+            }
         }
 
-        private synchronized void commit() {
-            if (this.cache.size() > 1) {
-                log("Committing to the db.");
-                this.cache.forEach(this.db::put);
-                this.cache.clear();
-                this.notifyAll();
-            } else {
-                log("Cache already has space, nothing to commit.");
+        private void commit() {
+            synchronized (this) {
+                if (this.cache.size() > 1) {
+                    log("Committing to the db");
+                    this.cache.forEach(this.db::put);
+                    this.cache.clear();
+                    this.notifyAll();
+                } else {
+                    log("Cache already has space, nothing to commit");
+                }
             }
         }
 
@@ -225,56 +352,65 @@ public class Main {
     }
 
     static class PutWorker<K, V> extends Thread {
-        private CacheMap<K, V> cache;
+        private final CacheMap<K, V> cacheMap;
         private K k;
         private V v;
 
-        PutWorker(String workerName, CacheMap<K, V> cache, K k, V v) {
+        PutWorker(String workerName, final CacheMap<K, V> cacheMap, K k, V v) {
             super(workerName);
-            this.cache = cache;
+            this.cacheMap = cacheMap;
             this.k = k;
             this.v = v;
         }
 
         @Override
         public void run() {
-            log("Worker " + this.getName() + " running.");
-            this.cache.put(this.k, v);
+            log("Worker " + this.getName() + " running");
+            V insertedValue = this.cacheMap.put(this.k, this.v);
+            if (Objects.isNull(insertedValue)) {
+                throw new RuntimeException("Returned value for the key " + this.k + " is null");
+            }
         }
     }
 
-    static class RemoveWorker<K, V> implements Runnable {
-        private CacheMap<K, V> cache;
+    static class RemoveWorker<K, V> extends Thread {
+        private CacheMap<K, V> cacheMap;
         private K k;
 
-        RemoveWorker(CacheMap<K, V> cache, K k) {
-            this.cache = cache;
+        RemoveWorker(String workerName, CacheMap<K, V> cacheMap, K k) {
+            super(workerName);
+            this.cacheMap = cacheMap;
             this.k = k;
         }
 
         @Override
         public void run() {
-            this.cache.remove(this.k);
+            log("Worker " + this.getName() + " running");
+            V v = this.cacheMap.remove(this.k);
+            ;
+            if (Objects.isNull(v)) {
+                log("Worker " + this.getName() + " could not remove the value");
+            }
         }
     }
 
-    static class GetWorker<K, V> extends Thread {
-        private CacheMap<K, V> cache;
+    private static class GetWorker<K, V> extends Thread {
+        private final CacheMap<K, V> cacheMap;
         private K k;
 
-        GetWorker(String workerName, CacheMap<K, V> cache, K k) {
+        GetWorker(String workerName, final CacheMap<K, V> cache, K k) {
             super(workerName);
-            this.cache = cache;
+            this.cacheMap = cache;
             this.k = k;
         }
 
         @Override
         public void run() {
             try {
-                log("Worker " + this.getName() + " running.");
-                V v = this.cache.get(this.k);
+                log("Worker " + this.getName() + " running");
+                V v = this.cacheMap.get(this.k);
                 if (Objects.isNull(v)) {
-                    throw new RuntimeException();
+                    log("Worker " + this.getName() + " did not find the value sought");
                 }
             } catch (InterruptedException e) {
                 e.printStackTrace();
@@ -301,14 +437,10 @@ public class Main {
                 try {
                     routine.run();
                 } catch (Throwable e) {
-                    log("Error in execution of the commit!");
+                    log("Error in execution of the commit");
                 }
             }, DEFAULT_INITIAL_DELAY_IN_MILLI, commitPeriod, TimeUnit.MILLISECONDS);
         }
-    }
-
-    private static boolean validateArgs(String... args) {
-        return args.length > 0 && args.length < 3;
     }
 
     private static void log(String str) {
